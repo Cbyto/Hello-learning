f2962d137b0687921ad821fbd2ac9c79f16707d4
Sebastián <63176256+Cbyto@users.noreply.github.com>
2025-11-28 00:37:06 -0300

    creando /services/product.services

---
+++ TalentoTech/Node-Express/Proyecto_ApiRest/services/product.service.js
@@ -0,0 +1,16 @@
+import { ProductModel } from '../models/product.model.js';
+
+export const ProductService = {
+  getAllProducts: async () => {
+    return await ProductModel.getAll();
+  },
+  getProductById: async (id) => {
+    return await ProductModel.getById(id);
+  },
+  createProduct: async (data) => {
+    return await ProductModel.create(data);
+  },
+  deleteProduct: async (id) => {
+    return await ProductModel.delete(id);
+  }
+};
