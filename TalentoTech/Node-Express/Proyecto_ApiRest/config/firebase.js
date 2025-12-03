d1e90d0248179740071be470f9a648515c02901d
Sebastián <63176256+Cbyto@users.noreply.github.com>
2025-12-03 19:11:00 -0300

    Actualización para aceptar vercel también

--- TalentoTech/Node-Express/Proyecto_ApiRest/config/firebase.js
+++ TalentoTech/Node-Express/Proyecto_ApiRest/config/firebase.js
@@ -2,17 +2,31 @@ import admin from 'firebase-admin';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+dotenv.config();
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync(path.join(__dirname, '../credenciales.json'), 'utf8')
-);
+let serviceAccount;
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+// LÓGICA HÍBRIDA:
+if (process.env.FIREBASE_CREDENTIALS) {
+  // CASO 1: Estamos en Vercel (o Producción) y leemos desde la variable de entorno
+  serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
+} else {
+  // CASO 2: Estamos en Local y leemos el archivo
+  const rutaArchivo = path.join(__dirname, '../credenciales.json');
+  if (fs.existsSync(rutaArchivo)) {
+    serviceAccount = JSON.parse(fs.readFileSync(rutaArchivo, 'utf8'));
+  } else {
+    console.error("❌ No se encontraron credenciales de Firebase.");
+  }
+}
+
+if (serviceAccount) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+}
 
 export const db = admin.firestore();
 export const auth = admin.auth();
